076b8d9508e2a35e135fb95783d78331f1d7f08e
Izzatbek <abdusharipovizzat03@gmail.com>
2025-07-21 13:07:52 +0500

    coding blog

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express=require('express');
-const {config}=require('dotenv');
+require('dotenv').config();
 const cors=require('cors');
 const cookieParser=require("cookie-parser");
 const db=require("./config/db.config");
 
 const app=express();
-config({encoding: 'latin1',debug: true,});
  
 app.use(express.json());
 app.use(cors({credentials:true,origin:"http://localhost:5173"}));
