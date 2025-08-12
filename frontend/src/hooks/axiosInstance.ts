f53c3090358c82d71278bced24b86d6158dc2931
Izzatbek Abdusharipov <135409718+IncridableAcuman@users.noreply.github.com>
2025-08-12 15:20:46 +0500

    Update axiosInstance.ts

--- frontend/src/hooks/axiosInstance.ts
+++ frontend/src/hooks/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance=axios.create({
     withCredentials:true,
-    baseURL:"http://localhost:8080/api"
+    baseURL:"https://izzatbek-blog.onrender.com"
 });
 
 axiosInstance.interceptors.request.use(
